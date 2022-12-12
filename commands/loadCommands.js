1e84c834597596d1094146d5bc1a91399e78a8c6
Antoine Bédard <antoinebedardqc@gmail.com>
2022-12-11 22:32:58 -0500

    Add /ping command

--- commands/loadCommands.js
+++ commands/loadCommands.js
@@ -33,7 +33,7 @@ exports.load = async (client) => {
         }
 
         try {
-            await command.execute(interaction);
+            await command.execute(interaction, client);
         } catch (error) {
             console.error(error);
             await interaction.reply({content: "There was an error while executing this command!", ephemeral: true});
--- commands/pingCommand.js
+++ commands/pingCommand.js
@@ -1,13 +1,19 @@
 "use strict";
 
 const {SlashCommandBuilder} = require("discord.js");
-
+const {EmbedBuilder} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with Pong!"),
-    async execute(interaction) {
-        await interaction.reply("Pong!");
+    async execute(interaction, client) {
+        const userLatency = (interaction.createdTimestamp - Date.now()) / 100;
+        const pingEmbed = new EmbedBuilder()
+            .setColor(0x00ffcb)
+            .setTitle("Pong")
+            .setDescription(`User latency : **${userLatency}**ms \n Bot latency :\xa0\xa0 **${client.ws.ping}**ms`);
+
+        await interaction.reply({embeds: [pingEmbed]});
     },
 };
