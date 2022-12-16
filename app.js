68ab7bc60088d1755b04aac18a7b0d1304de0462
Antoine Bédard <antoinebedardqc@gmail.com>
2022-12-16 16:18:42 -0500

    Add status

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 "use strict";
 
 require("dotenv").config();
-const {Client, GatewayIntentBits} = require("discord.js");
-const db = require("./models/dbSetup");
+const {Client, GatewayIntentBits, ActivityType} = require("discord.js");
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const db = require("./models/dbSetup");
 const commands = require("./commands/loadCommands");
 const events = require("./events/loadEvents");
 
 client.on("ready", async () => {
+    client.user.setPresence({
+        activities: [{
+            name: `Tom and Jerry!`,
+            type: ActivityType.Watching,
+        }],
+        status: "dnd",
+    });
+
     await commands.load(client);
     await events.load(client);
-    console.log(`\x1b[94mLogged in as ${client.user.tag}!\x1b[0m`);
+    console.info(`\x1b[94mLogged in as ${client.user.tag}!\x1b[0m`);
 });
 
 client.login(process.env.TOKEN);
