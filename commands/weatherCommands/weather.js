96d0135d39a5011cbde6e9bf447e8b88d36aaf4c
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-12-17 11:41:56 -0500

    Weather + DateTime function

--- commands/weatherCommands/weather.js
+++ commands/weatherCommands/weather.js
@@ -1,8 +1,10 @@
 const {
-    SlashCommandBuilder, EmbedBuilder
+    SlashCommandBuilder
 } = require('discord.js');
 const axios = require('axios');
-const getNextThreeDays = require('../../functions/DateTime');
+const getNextThreeDays = require('../../includes/functions/dateTimeUtils');
+const embedMsg = require("../../component/embedMessages");
+
 exports.command = {
     data: new SlashCommandBuilder()
         .setName('weather')
@@ -13,14 +15,10 @@ exports.command = {
                 .setDescription('City to get the weather from')
                 .setRequired(true)),
     async execute(interaction) {
-        const responseEmbed = new EmbedBuilder()
-            .setColor(0x00ffcb)
-            .setTitle("Weather")
-
         const city = interaction.options.getString('city');
         await axios.get(`https://goweather.herokuapp.com/weather/${city}`)
             .then(async (response) => {
-                if (response.data) {
+                if (response.data.temperature !== '') {
                     const nextThreeDays = getNextThreeDays();
                     let descriptionIcon = "";
                     switch (response.data.description) {
@@ -30,27 +28,58 @@ exports.command = {
                         case "Light snow":
                             descriptionIcon = '🌨'
                             break;
+                        case "Partly cloudy":
+                            descriptionIcon = '⛅'
+                            break;
+                        case "Clear":
+                            break;
+                        case "Patchy rain possible":
+                            descriptionIcon = '🌧'
+                            break;
+                        case "Heavy snow":
+                            descriptionIcon = '🌨'
+                            break;
+                        case "Snow":
+                            descriptionIcon = '🌨'
+                            break;
+                        case "Rain":
+                            descriptionIcon = '🌧'
+                            break;
+                        case "Light rain shower":
+                            descriptionIcon = '🌧'
+                            break;
                         default:
                             break;
                     }
-                    responseEmbed
-                        .setDescription(`**${city}**
-                        Temperature:\xa0\xa0**${response.data.temperature}** 
-                        Wind:\xa0\xa0**${response.data.wind}**
-                        Description:\xa0\xa0**${response.data.description} ${descriptionIcon}**
-
-                        Forecast:
-                        **${nextThreeDays[0]}** T: ${response.data.forecast[0].temperature} W: ${response.data.forecast[0].wind}
-                        **${nextThreeDays[1]}** T: ${response.data.forecast[1].temperature} W: ${response.data.forecast[1].wind}
-                        **${nextThreeDays[2]}** T: ${response.data.forecast[2].temperature} W: ${response.data.forecast[2].wind}
-                        `);
-                    await interaction.reply({ embeds: [responseEmbed] });
+                    await interaction.reply({
+                        embeds: [
+                            await embedMsg.infoMsg("Weather", `**${city}**
+                            Temperature:\xa0\xa0**${response.data.temperature}** 
+                            Wind:\xa0\xa0**${response.data.wind}**
+                            Description:\xa0\xa0**${response.data.description} ${descriptionIcon}**
+    
+                            Forecast:
+                            **${nextThreeDays[0]}** T: ${response.data.forecast[0].temperature} W: ${response.data.forecast[0].wind}
+                            **${nextThreeDays[1]}** T: ${response.data.forecast[1].temperature} W: ${response.data.forecast[1].wind}
+                            **${nextThreeDays[2]}** T: ${response.data.forecast[2].temperature} W: ${response.data.forecast[2].wind}
+                            `),
+                        ],
+                    });
+                } else {
+                    await interaction.reply({
+                        embeds: [
+                            await embedMsg.errorMsg("Weather", `The city is invalid, you may wanna try again using this format ex: **Regina, Saskatchewan**`),
+                        ],
+                    });
                 }
             })
             .catch(async (error) => {
                 if (error.response.data.message === 'NOT_FOUND') {
-                    responseEmbed.setDescription("The city is invalid, you may wanna try again.");
-                    await interaction.reply({ embeds: [responseEmbed] });
+                    await interaction.reply({
+                        embeds: [
+                            await embedMsg.errorMsg("Weather", `The city is invalid, you may wanna try again using this format ex: **Regina, Saskatchewan**`),
+                        ],
+                    });
                 }
             });
     }
